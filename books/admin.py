499cc2534cf8073438935b4a5383067fdc156b80
Artem <gameepicTV@gmail.com>
2023-11-20 00:04:35 +0600

    #6 Зарегистрировал модели в админке

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Book, Author, Genre, Review, Favorite
 
-# Register your models here.
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'publish_date', 'average_rating')
+    # Добавьте другие параметры админ-панели здесь по мере необходимости
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'birth_date')
+    # Дополнительные настройки админки для автора
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    # Настройки для жанра
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('book', 'user', 'rating', 'created_at')
+    # Настройки для отзывов
+
+
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('user', 'book', 'added_on')
+    # Настройки для списка избранных книг
